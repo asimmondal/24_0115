d97c6264ff6e31fc6423a9173e29a32ab75c3fcb
Mondal Asim <mandaru.asim@gmail.com>
2015-04-23 17:49:56 +0000

    sample.php

---
+++ sample.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = asim ;
+$age = 22 ;
+$color = red ;
+
+
+
+
+echo "welcome to  my website<br>" ;
+echo "are you enjoying it?<br>" ;
+echo "i hope you like the massege<br>"
+
+
+
+
+?>
+
